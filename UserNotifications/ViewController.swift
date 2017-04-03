2a574430bd093b6282660a5d2af9a2c9d4801e33
Junior Samaroo <jns0987654321@gmail.com>
2017-04-03 18:39:53 -0400

    Initial Commit

---
+++ UserNotifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotifications
+//
+//  Created by Junior Samaroo on 2017-04-03.
+//  Copyright © 2017 Junior Samaroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
